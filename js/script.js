bfb4747169b23926de63ff869077c068177e98f7
Jason Hargrove <rijaha@gmail.com>
2021-05-22 10:02:34 -0500

    w05d03 attendance commit.

--- js/script.js
+++ js/script.js
@@ -1,21 +1,13 @@
-// cached dom nodes
+
+// ========== console log everyting =========
+
+let log = console.log
+
+
+// ========== dom nodes ==========
 const firstChild = document.querySelector('.first-child');
 const secondChild = document.querySelector('.second-child');
 const thirdChild = document.querySelector('.third-child');
-
-
-firstChild.innerHTML = '<h1>Accumulative Dose</h1>';
-
-secondChild.innerHTML = '<p>Escape if you can!</p>';
-
-
-// button
-const newButton = document.createElement('button');
-
-newButton.innerHTML = 'Start the Game';
-
-thirdChild.appendChild(newButton);
-// end button
 
 
 
@@ -34,9 +26,59 @@ const updateButtons = () => {
 
 }
 
+// ========== classes ==========
 
-const startGame = () => {
+class Character {
+  constructor(name, exposure) {
+    this.name = name;
+    this.exposure = exposure;
+  }
+  pickUp(){
 
+  }
 }
 
-// document.body.onload = startGame;
+const player = new Character('Player', 10);
+
+const pen = new Character('Pen', 11);
+const adjustableWrench = new Character('Wrench', 12);
+const paper = new Character('Paper', 13);
+
+
+// ========== pages ==========
+
+// start - page 1
+const startGame = () => {
+
+  // title
+  firstChild.innerHTML = '<h1>Accumulative Dose</h1>';
+
+  // bottle
+  secondChild.innerHTML = '<p>Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. </p>';
+
+  // start button
+  const newButton = document.createElement('button');
+
+  newButton.classList.add('start-button');
+
+  newButton.innerHTML = 'Start the Game';
+
+  thirdChild.appendChild(newButton);
+  // end start button
+}
+
+// ========== on load ==========
+window.onload = startGame();
+
+const startButton = document.querySelector(".start-button");
+
+// ========== event listiners ==========
+startButton.addEventListener('click', function(evt) {
+  log(evt);
+});
+
+// ....not going to use overlay. use to toggl off initial page.
+// openButton.addEventListener("click", function() {
+//   modal.classList.toggle("closed");
+//   modalOverlay.classList.toggle("closed");
+// });
