0581d0b8d5e699de3a282b86c7d0a0eee01bfa65
Jason Hargrove <rijaha@gmail.com>
2021-05-22 12:25:05 -0500

    Started the pages.

--- js/script.js
+++ js/script.js
@@ -1,33 +1,42 @@
-
 // ========== console log everyting =========
-
 let log = console.log
 
-
 // ========== dom nodes ==========
-const firstChild = document.querySelector('.first-child');
-const secondChild = document.querySelector('.second-child');
-const thirdChild = document.querySelector('.third-child');
+// start page
+const startGridContainer = document.querySelector('.start-grid-container');
+const startFirstChild = document.querySelector('.start-first-child');
+const startSecondChild = document.querySelector('.start-second-child');
+const startThirdChild = document.querySelector('.start-third-child');
 
+// all the other pages
+const pageGridContainer = document.querySelector('.page-grid-container');
+const firstPageChild = document.querySelector('.first-page-child');
+const secondPageChild = document.querySelector('.second-page-child');
+const thirdPageChild = document.querySelector('.third-page-child');
 
+// death page. same format as the start page.
+const deathGridContainer = document.querySelector('.death-gird-container');
+const firstDeathChild = document.querySelector('.first-death-child');
+const secondDeathChild = document.querySelector('.second-death-child');
+const thirddDeathChild = document.querySelector('.third-death-child');
 
-// const thePlayer = {
-//   items: []
-// }
+// ========== global fuinctions ==========
+// increases dose randomly on page
+const randomDose = () => {
 
-// if title = someting ===>
-// changes: button, dose, etc.
+}
+
+// may use
 const turnThePage = () => {
   // render the page
 }
 
+// may use
 const updateButtons = () => {
-
 
 }
 
 // ========== classes ==========
-
 class Character {
   constructor(name, exposure) {
     this.name = name;
@@ -36,25 +45,29 @@ class Character {
   pickUp(){
 
   }
+  useItem() {
+
+  }
 }
 
+// ========== instantiate classes ==========
 const player = new Character('Player', 10);
 
 const pen = new Character('Pen', 11);
 const adjustableWrench = new Character('Wrench', 12);
 const paper = new Character('Paper', 13);
+const brokenPipe = new Character('Broken Pipe', 14);
 
 
 // ========== pages ==========
-
-// start - page 1
+// game start
 const startGame = () => {
 
   // title
-  firstChild.innerHTML = '<h1>Accumulative Dose</h1>';
+  startFirstChild.innerHTML = '<h1>Accumulative Dose</h1>';
 
   // bottle
-  secondChild.innerHTML = '<p>Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. </p>';
+  startSecondChild.innerHTML = '<p>Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. </p>';
 
   // start button
   const newButton = document.createElement('button');
@@ -63,8 +76,30 @@ const startGame = () => {
 
   newButton.innerHTML = 'Start the Game';
 
-  thirdChild.appendChild(newButton);
+  startThirdChild.appendChild(newButton);
   // end start button
+}
+
+// specific pages start here
+const firstPage = () => {
+  startGridContainer.classList.replace('start-grid-container', 'page-grid-container')
+
+
+}
+
+const secondPage = () => {
+
+
+}
+
+const thirdPage = () => {
+
+
+}
+
+const death = () => {
+
+
 }
 
 // ========== on load ==========
@@ -73,9 +108,7 @@ window.onload = startGame();
 const startButton = document.querySelector(".start-button");
 
 // ========== event listiners ==========
-startButton.addEventListener('click', function(evt) {
-  log(evt);
-});
+startButton.addEventListener('click', firstPage);
 
 // ....not going to use overlay. use to toggl off initial page.
 // openButton.addEventListener("click", function() {
