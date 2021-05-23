3d070e25a591905ce2e8e2b3f0140397b27b3eae
Jason Hargrove <rijaha@gmail.com>
2021-05-23 12:33:54 -0500

    Made global functions for left button and creating a div with text

--- js/script.js
+++ js/script.js
@@ -10,9 +10,8 @@ const startThirdChild = document.querySelector('.start-third-child');
 
 // all the other pages
 const pageGridContainer = document.querySelector('.page-grid-container');
-const firstPageChild = document.querySelector('.first-page-child');
-const secondPageChild = document.querySelector('.second-page-child');
-const thirdPageChild = document.querySelector('.third-page-child');
+
+
 
 // death page. same format as the start page.
 const deathGridContainer = document.querySelector('.death-gird-container');
@@ -25,6 +24,30 @@ const thirddDeathChild = document.querySelector('.third-death-child');
 const replaceClass = (selector, old, updated) => {
     selector.classList.replace(old, updated);
 }
+
+
+// makes the big text box and places text into it
+const fourthChildDiv = (text) => {
+  const createDiv = document.createElement('div');
+  createDiv.classList.add('fourth-page-child');
+  createDiv.innerHTML = text;
+  startGridContainer.appendChild(createDiv);
+}
+
+
+
+// makes a left button
+const createLeftButton = () => {
+  const thirdPageChild = document.querySelector('.third-page-child');
+  const leftButton = document.createElement('button');
+  leftButton.classList.add('left-button');
+  leftButton.innerHTML = 'Go Left';
+  thirdPageChild.appendChild(leftButton);
+}
+
+
+
+
 
 // increases dose randomly on page
 const randomDose = () => {
@@ -64,33 +87,32 @@ const paper = new Character('Paper', 13);
 const brokenPipe = new Character('Broken Pipe', 14);
 
 
-// ========== pages ==========
-// game start
+// ==================== pages ====================
+
+// ===== game start ======
 const startGame = () => {
 
   // title
   startFirstChild.innerHTML = '<h1>Accumulative Dose</h1>';
 
   // bottle
-  startSecondChild.innerHTML = '<p>Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. </p>';
+  startSecondChild.innerHTML = '<p>Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive.Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive.Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive.Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive.</p>';
 
   // start button
   const newButton = document.createElement('button');
-
   newButton.classList.add('start-button');
-
   newButton.innerHTML = 'Start the Game';
-
   startThirdChild.appendChild(newButton);
   // end start button
 }
 
+// ===== page 1 =====
 // specific pages start here
 const firstPage = () => {
   // replace grid container
   replaceClass(startGridContainer, 'start-grid-container', 'page-grid-container');
 
-// replace first child - dose meter
+  // replace first child - dose meter
   replaceClass(startFirstChild, 'start-first-child', 'first-page-child');
 
   // replace Second - text?
@@ -99,16 +121,34 @@ const firstPage = () => {
   // replace third
   replaceClass(startThirdChild, 'start-third-child', 'third-page-child');
 
+  // content - first - dose rate
+  const firstPageChild = document.querySelector('.first-page-child');
+  firstPageChild.innerHTML = `DOSE ${player.uptake}`;
 
-// firstPageChild.innerHTML =
-// `<h3>DOSE</h3> <center>${player.uptake}</center>`;
+  // content - second - text
+  const secondPageChild = document.querySelector('.second-page-child');
+  secondPageChild.innerHTML = `Hi.You see a ${pen.name}`;
+
+  // content - third - item list
+  const thirdPageChild = document.querySelector('.third-page-child');
+
+  // removes the start button
+  startButton.remove();
+
+    // create - left button on third page child.
+  createLeftButton();
+
+  // create a fourth div - big text.
+  fourthChildDiv('Hi. I\'m text. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices. Survive. Escape if you can! Make the right choices.');
 
 
-// startSecondChild.innerHTML = `Cool. It worked!`;
 
 
+// so...crate two more buttons. remove the start button - maybe do that first.
+// repalce the text.
+// make an items menu.
 
-// end  first page
+
 }
 
 const secondPage = () => {
@@ -127,15 +167,9 @@ const death = () => {
 }
 
 // ========== on load ==========
-window.onload = startGame();
+startGame();
 
 const startButton = document.querySelector(".start-button");
 
 // ========== event listiners ==========
 startButton.addEventListener('click', firstPage);
-
-// ....not going to use overlay. use to toggl off initial page.
-// openButton.addEventListener("click", function() {
-//   modal.classList.toggle("closed");
-//   modalOverlay.classList.toggle("closed");
-// });
